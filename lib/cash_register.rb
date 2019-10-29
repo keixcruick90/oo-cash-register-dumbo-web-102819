c995ca3a98c553b6e73021aef0e5963e1520a8db
Keillan Cruickshank <cruickshankxkeillan90@gmail.com>
2019-10-29 20:11:22 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,18 @@
+class CashRegister
+  attr_accessor :coupon
 
+    def checkout
+      total = 0
+      #the shopping_cart method holds an array of all the user's items
+      shopping_cart.each do |item|
+        total += item.price
+      end
+
+      if coupon
+        total = total - total * coupon / 100.00
+      end
+
+      total
+
+    end
+end
